9d82b59d0d670738efb5cad2bd355a09f0871987
JP Juliao <jpjuliao@gmail.com>
2022-11-22 14:02:52 -0500

    second

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,38 +4,60 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
+var context = null;
+var oscillator = null;
+function getOrCreateContext() {
+  if (!context) {
+    context = new AudioContext();
+    oscillator = context.createOscillator();
+    oscillator.connect(context.destination);
+  }
+  return context;
+  
+}
+
+        var isStarted = false;
+        function playSound(frequency, type) {
+          getOrCreateContext();
+          oscillator.frequency.setTargetAtTime(frequency, context.currentTime, 0);
+          if (!isStarted) {
+            oscillator.start(0);
+            isStarted = true;
+          } else {
+            context.resume();
+          }
+	  setTimeout(context.suspend, 500);
+        }
+
+        function stopSound() {
+          context.suspend();
+        }
+
+        document.getElementById('basic').addEventListener('click', playSound.bind(null, 440, 'square'));
+        document.getElementById('basic2').addEventListener('click', playSound.bind(null, 800, 'square'));
+        document.getElementById('basic3').addEventListener('click', playSound.bind(null, 1000, 'square'));
+        document.getElementById('stop').addEventListener('click', stopSound);
+
+
 </script>
 
 <template>
 
-  <h1 className="text-3xl font-bold underline transition-all transition ease-in-out delay-150 hover:bg-black">
-        {{ msg }}
-	</h1>
+<div class="flex flex-col h-screen items-center justify-center content-center text-center">
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <h1 className="text-3xl flex flex-1 basis-1/2 items-center justify-center content-center w-screen h-1/2 font-bold underline transition-all ease-in-out delay-150 bg-blue-500 text-white p-1 hover:p-5 hover:text-xl hover:bg-yellow-500">
+  <span class="flex items-center justify-center content-center rounded-full inline-block p-3 font-bold underline transition-all ease-in-out delay-150 bg-black min-h-[30%] min-w-[30%] text-white group-hover:text-xl hover:bg-red-500 hover:min-h-[50%] hover:min-w-[50%]" >
+  {{ msg }}</span>
+  </h1>
+
+  <div class="group flex flex-1 items-center basis-1/2  justify-center content-center text-3xl font-bold underline transition-all ease-in-out delay-150 bg-red-500 h-1/2 w-screen hover:text-xl hover:bg-black">
+    <button class="text-3xl rounded-full p-3 font-bold underline transition-all ease-in-out delay-150 bg-black min-h-[30%] min-w-[30%] text-white group-hover:text-xl hover:bg-red-500 hover:min-h-[50%] hover:min-w-[50%]" type="button" @click="count++">{{ count }}</button>
+  </div>
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+
 </style>
